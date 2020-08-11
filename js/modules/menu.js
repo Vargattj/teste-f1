71770afb1c3648e59c571a0306f1981a151e9386
vargattj <vargattjh@gmail.com>
2020-08-10 23:03:48 -0300

    finishing teste

---
+++ js/modules/menu.js
@@ -0,0 +1,19 @@
+export default function iniciarMenu() {
+  let show = true;
+
+  const categorias = document.querySelector(".categorias");
+  const menuToggle = document.querySelector(".menu-burguer");
+
+  menuToggle.addEventListener("click", () => {
+    document.body.style.overflow = show ? "hidden" : "initial";
+    categorias.classList.toggle("on", show);
+    show = !show;
+  });
+
+  categorias.addEventListener("click", (e) => {
+    if (e.target === categorias) {
+      categorias.classList.toggle("on", show);
+    }
+    show = !show;
+  });
+}
---
+++ js/script.js
@@ -0,0 +1,5 @@
+import iniciarDropdown from './modules/dropdown.js';
+import iniciarMenu from './modules/menu.js';
+
+iniciarDropdown();
+iniciarMenu();
